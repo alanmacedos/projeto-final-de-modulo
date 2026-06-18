ffd8fa2f0458fca57eff74cf8bdbe6836ddb1b56
alanmacedos <alan.m.silva16@gmail.com>
2026-06-18 16:52:55 -0300

    objects creating

--- script.js
+++ script.js
@@ -1,5 +1,70 @@
-const appliance = {
-    name : "Compressor",
+const elevacar = {
+    name : "Elevacar",
+    watts : 3000,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 3,
+};
+
+const ventiladorSolaster = {
+    name : "Ventilador Solaster",
+    watts : 270,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 1,
+};
+
+const ventiladorTron = {
+    name : "Ventilador Tron",
+    watts : 140,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 1,
+};
+
+const ventiladorArge = {
+    name : "Ventilador Arge MAX",
+    watts : 150,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 1,
+};
+
+const filtroDeAgua = {
+    name : "Filtro de Água",
+    watts : 54,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 1,
+};
+
+const calibradorDePneu = {
+    name : "Calibrador de Pneu",
+    watts : 8,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 2,
+
+};
+
+const lampadas = {
+    name : "Lâmpadas",
+    watts : 72,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 58,
+};
+
+const carregadorDeBateria = {
+    name : "Carregador de bateria",
+    watts : 120,
+    hoursPerDay : 2,
+    daysPerMonth : 20,
+    quantity : 1,
+};
+
+const balanceadorDeRoda = {
+    name : "Balanceador de roda",
     watts : 1500,
     hoursPerDay : 2,
     daysPerMonth : 20
